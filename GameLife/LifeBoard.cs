33a855feb8d8316331f040c39cbb8169e739fa31
LyzhinIvan <lyzhin-ivan@yandex.ru>
2015-04-28 11:10:23 +0500

    First version after 1 hour of coding

---
+++ GameLife/LifeBoard.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace GameLife
+{
+    public class LifeBoard
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public bool[,] Board;
+
+        public LifeBoard(int width=10, int height=10)
+        {
+            Width = width;
+            Height = height;
+            Board = new bool[width,height];
+        }
+
+        public void GoToNext()
+        {
+            bool[,] newBoard = new bool[Width,Height];
+            for(int i=0; i<Width; ++i)
+                for (int j = 0; j < Height; ++j)
+                {
+                    int nearCount = NearCount(new Cell(i, j));
+                    if (Board[i, j])
+                    {
+                        if (nearCount == 2 || nearCount == 3)
+                            newBoard[i, j] = true;
+                    }
+                    else if (nearCount == 3)
+                        newBoard[i, j] = true;
+                }
+            Board = newBoard;
+        }
+
+        private int NearCount(Cell cell)
+        {
+            int count = 0;
+            for(int i=-1; i<=1; ++i)
+                for (int j = -1; j <= 1; ++j)
+                {
+                    if (i == 0 && j == 0) continue;
+                    if (CheckBounds(new Cell(cell.X+i, cell.Y+j)) && Board[cell.X + i, cell.Y + j])
+                        count++;
+                }
+            return count;
+        }
+
+        private bool CheckBounds(Cell cell)
+        {
+            return cell.X >= 0 && cell.X < Width && cell.Y >= 0 && cell.Y < Height;
+        }
+
+        public class Cell
+        {
+            public int X { get; private set; }
+            public int Y { get; private set; }
+
+            public Cell(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    }
+}
---
+++ GameLife/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Timers;
+using System.Windows.Threading;
+
+namespace GameLife
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const int Rows = 20, Columns = 20;
+        private LifeBoard _lifeBoard;
+        private Border[,] _borders;
+        private Timer timer;
+        private Dispatcher dispatcher;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new Timer();
+            timer.Elapsed += TimerOnElapsed;
+            timer.Interval = 1000;
+            for(int i=0; i<Rows; ++i)
+                BoardGrid.RowDefinitions.Add(new RowDefinition());
+            for(int i=0; i<Columns; ++i)
+                BoardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            _borders = new Border[Rows,Columns];
+            BoardGrid.Width = Rows*20;
+            BoardGrid.Height = Rows*20;
+            for(int i=0; i<Rows; ++i)
+                for (int j = 0; j < Columns; ++j)
+                {
+                    Border border = new Border();
+                    border.Width = 20;
+                    border.Height = 20;
+                    border.BorderBrush = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                    border.BorderThickness = new Thickness(1);
+                    border.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    border.MouseLeftButtonDown += BorderOnMouseLeftButtonDown;
+                    Grid.SetRow(border, i);
+                    Grid.SetColumn(border, j);
+                    BoardGrid.Children.Add(border);
+                    _borders[i, j] = border;
+                }
+            _lifeBoard = new LifeBoard(Rows, Columns);
+        }
+
+        private void TimerOnElapsed(object sender, ElapsedEventArgs elapsedEventArgs)
+        {
+            //timer.Enabled = false;
+            _lifeBoard.GoToNext();
+            //RefreshBoard();
+            Dispatcher.Invoke(RefreshBoard);
+        }
+
+        private void BorderOnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            Border border = (Border) sender;
+            int x = Grid.GetRow(border);
+            int y = Grid.GetColumn(border);
+            _lifeBoard.Board[x, y] = !_lifeBoard.Board[x, y];
+            RefreshBoard();
+        }
+
+        private void RefreshBoard()
+        {
+            try
+            {
+                foreach (var obj in BoardGrid.Children)
+                {
+                    Border b = obj as Border;
+                    int i = Grid.GetRow(b);
+                    int j = Grid.GetColumn(b);
+                    if (_lifeBoard.Board[i, j])
+                        b.Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+                    else b.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void BtnStart_OnClick(object sender, RoutedEventArgs e)
+        {
+            timer.Enabled = true;
+        }
+
+        private void BtnStop_OnClick(object sender, RoutedEventArgs e)
+        {
+            timer.Enabled = false;
+        }
+    }
+}
